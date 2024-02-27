6eb23334a7c1ad1f8a0fa61abe76d3ba9f9f31f7
reyan2541 <anathe2541@gmail.com>
2024-02-27 22:43:24 +0300

    my first

---
+++ practice.js
@@ -0,0 +1,36 @@
+document.write("hello pu");
+document.write("hello every one");
+var x=90;
+var y=45;
+document.write(x*y);
+var month=1;
+switch(month)
+{
+    case 1:
+        document.write("janury");
+        break;
+    case 2:
+        document.write("feb"); 
+        break;
+    case 3:
+        document.write("march");
+        break;
+    case 4:
+        document.write("april");
+        break;
+    case 5:
+        document.write("may");
+        break;
+    case 6:
+        document.write("june");
+        break;
+    case 7:
+        document.write("july");
+        break;
+    case 8:
+        document.write("aug");
+        break;
+    case 9:
+        document.write("sep");   
+        break;        
+}
